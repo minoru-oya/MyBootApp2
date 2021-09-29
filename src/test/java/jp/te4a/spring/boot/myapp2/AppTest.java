e02a1ed787462fa05234fe43942945d7ecbfed1d
minoru-oya <83932690+minoru-oya@users.noreply.github.com>
2021-09-29 11:09:45 +0900

    testA

--- src/test/java/jp/te4a/spring/boot/myapp2/AppTest.java
+++ src/test/java/jp/te4a/spring/boot/myapp2/AppTest.java
@@ -1,11 +1,13 @@
 package jp.te4a.spring.boot.myapp2;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
+ * aaaaaaaaaa
  */
 public class AppTest 
 {
